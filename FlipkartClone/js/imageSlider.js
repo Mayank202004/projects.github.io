1bc1dd64cc5e41028202ce3490db0f2841eab9d9
Mayank202004 <136550347+Mayank202004@users.noreply.github.com>
2023-06-14 10:53:35 +0530

    Added new file (Flipkart clone)

---
+++ FlipkartClone/js/imageSlider.js
@@ -0,0 +1,14 @@
+// export const imageSlider = [
+//     {
+//         img :"https://rukminim1.flixcart.com/fk-p-flap/1688/280/image/b3d109166fa95608.jpg?q=50",
+//         link :"https://www.flipkart.com/mobiles/~cs-817ef5cp9m/pr?sid=tyy%2C4io&collection-tab-name=Samsung+F14&param=8768&otracker=hp_bannerads_1_2.bannerAdCard.BANNERADS_a_QVHXSYTYYLE9" 
+//     },
+//     {
+//         img :"https://rukminim1.flixcart.com/fk-p-flap/1688/280/image/896ed9280bc59601.jpg?q=50",
+//         link :"https://www.flipkart.com/travel/flights?param=FKDTHPWNOV223AIRLINESNKDJBDJKKJUYBBHDFSa&otracker=hp_bannerads_2_2.bannerAdCard.BANNERADS_Travel-DTHPW-20feb-INTL-Airasia_EO44N0IM8UIH" 
+//     },
+//     {
+//         img :"https://rukminim1.flixcart.com/fk-p-flap/1688/280/image/9b53bd00f765e2d7.jpg?q=50",
+//         link : "https://www.flipkart.com/travel/flights?param=FKHPtravelHPWEMIfederal1299off3023asqwweqwwe&otracker=hp_bannerads_3_2.bannerAdCard.BANNERADS_zerofee_2KKZOFPIEWBN"
+//     }
+// ]
---
+++ FlipkartClone/js/script.js
@@ -0,0 +1,13 @@
+// import{imageSlider} from "./imageSlider.js"
+// // =============== img slider ================
+// let a = document.querySelector(".imgsliderlist")
+// let b = ''
+// console.log(imageSlider)
+// imageSlider.forEach(el =>{
+//     b +=`
+//     <div class="imgslideritem">
+//         <a href="${el.link}"></a>
+//         <img src="${el.img}" alt="">
+//     </div>`
+// })
+// a.innerHTML = b;
